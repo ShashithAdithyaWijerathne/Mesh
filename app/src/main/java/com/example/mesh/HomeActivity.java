e269e3f56e823b6725c63f2fcc42261fca77cf0f
HbsAdithya <48553417+HbsAdithya@users.noreply.github.com>
2020-04-25 00:45:25 +0530

    Adding Admob Banner Ads into project

--- app/src/main/java/com/example/mesh/HomeActivity.java
+++ app/src/main/java/com/example/mesh/HomeActivity.java
@@ -2,13 +2,34 @@ package com.example.mesh;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.paperdb.Paper;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button LogoutButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        LogoutButton = (Button) findViewById(R.id.Logout_Button);
+
+        LogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Paper.book().destroy();//Delete every values in cache memory
+
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
